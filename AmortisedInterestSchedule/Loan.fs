2aa9c7d2cd4bd02d7ca8e376cfa89103e4fdd5d5
Lucas Bruck <lukabruck@gmail.com>
2024-09-02 15:33:50 +0100

    [SCHEDULE] Make the instalment be a concept returned by the loan type

--- AmortisedInterestSchedule/Loan.fs
+++ AmortisedInterestSchedule/Loan.fs
@@ -4,6 +4,21 @@ open System
 open AmortisedInterestSchedule.Term
 open AmortisedInterestSchedule.Rate
 
+type Instalment =
+    { OpeningBalance: float
+      ClosingBalance: float
+      Principal: float
+      Interest: float }
+
+    member this.Total = this.Principal + this.Interest
+
+    member this.WithRounding(places: int) =
+        { this with
+            OpeningBalance = Math.Round(this.OpeningBalance, places)
+            ClosingBalance = Math.Round(this.ClosingBalance, places)
+            Principal = Math.Round(this.Principal, places)
+            Interest = Math.Round(this.Interest, places) }
+
 type Loan =
     { Amount: double
       Term: Term
@@ -20,45 +35,30 @@ type Loan =
         * ((this.GetRate * ((1.0 + this.GetRate) ** this.GetTerm))
            / (((1.0 + this.GetRate) ** (this.GetTerm |> float)) - 1.0))
 
-type Instalment =
-    { OpeningBalance: float
-      ClosingBalance: float
-      Principal: float
-      Interest: float }
+    member private this.Instalments' (outstandingBalance: float) periods (rate: float) amortisedPaymentAmount instalments =
+        if List.length instalments = periods then
+            instalments
+        else
+            let interestAccruedInPeriod = rate * outstandingBalance
+            let balanceWithInterestApplied = interestAccruedInPeriod + outstandingBalance
 
-    member this.Total = this.Principal + this.Interest
+            let principalComponent =
+                if List.length instalments = (periods - 1) then
+                    balanceWithInterestApplied - interestAccruedInPeriod
+                else
+                    (amortisedPaymentAmount - interestAccruedInPeriod)
 
-    member this.WithRounding(places: int) =
-        { this with
-            OpeningBalance = Math.Round(this.OpeningBalance, places)
-            ClosingBalance = Math.Round(this.ClosingBalance, places)
-            Principal = Math.Round(this.Principal, places)
-            Interest = Math.Round(this.Interest, places) }
+            let actualPaymentAmount = principalComponent + interestAccruedInPeriod
+            let closingBalancePostPayment = balanceWithInterestApplied - actualPaymentAmount
 
-let rec private ToInst' (outstandingBalance: float) periods (rate: float) amortisedPaymentAmount instalments =
-    if List.length instalments = periods then
-        instalments
-    else
-        let interestAccruedInPeriod = rate * outstandingBalance
-        let balanceWithInterestApplied = interestAccruedInPeriod + outstandingBalance
+            let nextInstalment =
+                { OpeningBalance = outstandingBalance
+                  Interest = interestAccruedInPeriod
+                  Principal = principalComponent
+                  ClosingBalance = closingBalancePostPayment }
 
-        let principalComponent =
-            if List.length instalments = (periods - 1) then
-                balanceWithInterestApplied - interestAccruedInPeriod
-            else
-                (amortisedPaymentAmount - interestAccruedInPeriod)
+            this.Instalments' nextInstalment.ClosingBalance periods rate amortisedPaymentAmount (nextInstalment :: instalments)
 
-        let actualPaymentAmount = principalComponent + interestAccruedInPeriod
-        let closingBalancePostPayment = balanceWithInterestApplied - actualPaymentAmount
-
-        let nextInstalment =
-            { OpeningBalance = outstandingBalance
-              Interest = interestAccruedInPeriod
-              Principal = principalComponent
-              ClosingBalance = closingBalancePostPayment }
-
-        ToInst' nextInstalment.ClosingBalance periods rate amortisedPaymentAmount (nextInstalment :: instalments)
-
-let ToInstalments loan =
-    ToInst' loan.Amount (loan.GetTerm |> int) loan.GetRate loan.AmortizedPaymentAmount []
-    |> List.rev
+    member this.Instalments =
+        this.Instalments' this.Amount (this.GetTerm |> int) this.GetRate this.AmortizedPaymentAmount []
+        |> List.rev
