5110a2ab8cea4243da63e162ed29278bea5f95eb
Lucas Bruck <lukabruck@gmail.com>
2024-09-24 15:08:25 +0100

    [SCHEDULE] Tighten assertions on schedule

--- AmortisedInterestScheduleTest/ScheduleTests.fs
+++ AmortisedInterestScheduleTest/ScheduleTests.fs
@@ -57,9 +57,10 @@ let ``A 12 month loan, disbursed on 1 Jan 2024, at 5% should be paid in full by 
         { Term = Term 12s
           Amount = 1000.0
           Rate = { Amount = 0.05; Frequency = Yearly } }
-        
+
     // Indicative cost
-    let indicativeCost = loan.IndicativeSchedule |> List.map (fun i -> i.Total) |> List.sum
+    let indicativeCost =
+        loan.IndicativeSchedule |> List.map (fun i -> i.Total) |> List.sum
 
     // Just parse it lol
     let disbursalDate = DateTime.Parse "2024-01-01T00:00:00Z"
@@ -80,29 +81,70 @@ let ``A 12 month loan, disbursed on 1 Jan 2024, at 5% should be paid in full by 
     Assert.Equal(1, List.head dueDates |> (fun i -> i.Day))
     Assert.Equal(2, List.head dueDates |> (fun i -> i.Month))
     Assert.Equal(2024, List.head dueDates |> (fun i -> i.Year))
-    
+
     // Date Validation of end
     Assert.Equal(1, List.last dueDates |> (fun i -> i.Day))
     Assert.Equal(1, List.last dueDates |> (fun i -> i.Month))
     Assert.Equal(2025, List.last dueDates |> (fun i -> i.Year))
-    
+
     // Actually test the schedule
     let disbursedSchedule = disbursed.schedule
     let disbursedCost = disbursedSchedule |> List.map (fun i -> i.Total) |> List.sum
-    
+
     // Compare indicative and disbursed
     let firstDue = List.head disbursedSchedule
     let quoted = List.head loan.IndicativeSchedule
-    
+
     Assert.Equal(quoted.Total, firstDue.Total)
     
-    // Some tolerance
+    // Tolerance of 1/365
     let errorRange value = (1. / 365.) * value
-    
+
     // Given the quote uses some very "applied" calculations, we cannot actually quote to a p/cent.
     // This is also due to the fact years are kinda 365.25 days long.
     // Given we're within 0.27% (.25 days), I'm okay.
-    Assert.InRange(disbursedCost,
-                   ( indicativeCost - errorRange indicativeCost),
-                   (indicativeCost + errorRange indicativeCost))
+    Assert.InRange(
+        disbursedCost,
+        (indicativeCost - errorRange indicativeCost),
+        (indicativeCost + errorRange indicativeCost)
+    )
+
+
+[<Fact>]
+let ``At 18.71% for 1500 at 12 months`` () =
+    // Based off a real loan
+
+    let loan =
+        { Term = Term 12s
+          Amount = 1500.0
+          Rate = { Amount = 0.1871; Frequency = Yearly } }
+
+    let disbursalDate = DateTime.Parse("2023-11-17")
+
+    let disbursedLoan = ofQuote loan disbursalDate
+
+    let disbursedTotalCost =
+        disbursedLoan
+        |> (fun l -> l.schedule)
+        |> List.map (fun i -> i.Total)
+        |> List.sum
+
     
+    // Tolerance of 1/365
+    let errorRange value = (1. / 365.) * value
+    let providedValue = 1656.97
+
+    Assert.InRange(
+        disbursedTotalCost,
+        (providedValue - errorRange providedValue),
+        (providedValue + errorRange providedValue)
+    )
+
+    let installmentAmount =
+        disbursedLoan.schedule
+        |> List.map (fun i -> i.Total)
+        |> List.groupBy id
+        |> List.map (fun (g, _) -> Math.Round(g, 2))
+        |> List.head
+
+    Assert.Equal(138.03, installmentAmount)
